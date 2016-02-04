6b8696d1cfe570e93a938de099248fdc3cbedc9d
Hardik Jagda <hardik.jagda@tinyowl.co.in>
2016-02-05 01:25:50 +0530

    Create standard gulp and gulp-config files. Write sass and watch tasks.

---
+++ gulp-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    path: {
+        sassRoot: './sass/',
+        sass: './sass/**/*.scss',
+        dist: './dist/',
+        sourcemaps: './'
+    },
+
+    fileNames: {
+        sassMain: 'app.scss',
+        appCSS: 'app.css'
+    }
+};
